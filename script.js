7f44027c4d3cdefdf599032490d18a6b0aec1257
Hadiiz <hadiziadeh@hotmail.com>
2021-02-23 17:12:13 +0200

    Clear button Works!

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.getElementById("cells-grid");
-
+const clear = document.getElementById("clear");
+let size;
+clear.addEventListener('click',function(){
+    do{
+        size = prompt("Enter a new Size(1-64)");
+    }while(isNaN(size) || (size<1 || size>64));
+    container.innerHTML = '';
+    generate_cells(size);
+})
 function generate_cells(num){
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     for(let i=0; i<num; i++){
@@ -16,5 +24,6 @@ function change_color(e){
     let color = Math.floor(Math.random()*16777215).toString(16);
     e.target.style.backgroundColor = `#${color}`;
 }
-generate_cells(2);
+// generate_cells(2);
 console.log(container);
+console.log(clear);
